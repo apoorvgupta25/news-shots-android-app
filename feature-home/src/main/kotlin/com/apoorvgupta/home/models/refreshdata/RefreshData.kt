0c9fd5373ef77c2bf7462ac4d2da43ff11953cfd
apoorvgupta25 <apurvagupta45@gmail.com>
2024-05-26 18:46:20 +0530

    ktlint formatting

--- feature-home/src/main/kotlin/com/apoorvgupta/home/models/refreshdata/RefreshData.kt
+++ feature-home/src/main/kotlin/com/apoorvgupta/home/models/refreshdata/RefreshData.kt
@@ -1,8 +1,7 @@
 package com.apoorvgupta.home.models.refreshdata
 
 import com.apoorvgupta.core.utils.EMPTY_STRING
-import com.google.gson.annotations.SerializedName
 
 data class RefreshData(
-   val str: String = EMPTY_STRING,
+    val str: String = EMPTY_STRING,
 )
